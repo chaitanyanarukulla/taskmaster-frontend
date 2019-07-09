bfb211136c86742313e3c551e57147a9e26655b0
Chai.Narukulla <chaitanyanarukulla@gmail.com>
2019-07-09 15:39:14 -0700

    test

--- taskmaster-frontend/src/components/tasks.js
+++ taskmaster-frontend/src/components/tasks.js
@@ -1,19 +1,15 @@
 import React, {useState, useEffect} from 'react';
-const API =   "http://localhost:5000";
+const API =   "http://taskmaster-dev-1.us-west-1.elasticbeanstalk.com/";
 
 function Tasks(){
-
   const [tasks, setTasks] = useState([])
-
   const _getTasks = () => {
     fetch( API + "/tasks", { mode : 'cors'})
     .then(data => data.json())
     .then(task => setTasks(task))
     .catch(console.error);
   }
-
   useEffect(_getTasks, []);
-
   return (
     <>
     <table>
@@ -23,6 +19,7 @@ function Tasks(){
           <th>Description</th>
           <th>Assignee</th>
           <th>Status</th>
+          <th>Pic</th>
         </tr>
         {tasks.map( (task) =>
           <tr id="task" key={task.id}>
@@ -30,6 +27,7 @@ function Tasks(){
             <td>{task.description}</td>
             <td>{task.assignee}</td>
             <td>{task.status}</td>
+            <td>{task.pic}</td>
           </tr>
           )}
       </tbody>
@@ -37,6 +35,29 @@ function Tasks(){
     </table>
     </>
   );
+}
+
+function Task() {
+  return (
+    <>
+      <header>This is the Task Master!</header>
+      <main>
+        <Tasks />
+      </main>
+      <footer>&copy; 2019 Chai</footer>
+     </>
+  );
+}
+
+class AddImage extends React.Component{
+  render(){
+    return(
+      <form action={`${API}/${this.props.id}/images`} method="post" encType="multipart/form-data">
+        <input name="file" type="file"></input>
+        <input name="submit" type="submit"></input>
+      </form>
+    )
+  }
 }
 
 export default Tasks;
---
+++ taskmaster-frontend/src/components/upload.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const API =   "http://localhost:5000";
+
+class AddImage extends React.Component{
+  render(){
+    return(
+      <form action={API + "/tasks/"  + this.props.id + "/images"} method="post" encType="multipart/form-data">
+        <input name="file" type="file"></input>
+        <input name="submit" type="submit"></input>
+      </form>
+    )
+  }
+}
+
+export default AddImage;
